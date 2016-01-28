1453b193d8d367b6967a45a269e551eb07b8716d
Junjun Zhang <junjun.ca@gmail.com>
2016-01-27 21:50:40 -0500

    use find_packages in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'ega_sub',
     description = 'Python tool for assisting EGA metadata submission',
-    packages=['ega_submission'],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires = ['Click', 'PyYAML', 'xmltodict'],
     entry_points={
         'console_scripts': [
